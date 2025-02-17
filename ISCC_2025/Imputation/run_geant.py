8c46a01f14b318b50befd0988b4f14f33e9f027c
FLYingK <118822513+FILingK@users.noreply.github.com>
2025-02-17 21:11:18 +0800

    Update run_geant.py

--- ISCC_2025/Imputation/run_geant.py
+++ ISCC_2025/Imputation/run_geant.py
@@ -1,3 +1,4 @@
+
 import os
 import subprocess
 
@@ -6,14 +7,14 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 seq_len = 96
 model = "TM-LLM"
-pred_len = 0
 percent = 100
 mask_rate = "0.5"
-train_epochs ="10"
-itr = "3"
+train_epochs ="1"
+itr = "1"
 llm_model = "gpt2"
-samplenum =5000 # number of training samples
-Lambda = 5
+sample_num =2000 # number of training samples
+Lambda = 4
+
 command = [
     "/root/miniconda3/bin/python", "run.py",
     "--train_epochs", train_epochs,
@@ -22,12 +23,11 @@ command = [
     "--is_training", "1",
     "--root_path", r"../datasets/net_traffic/GEANT",
     "--data_path", "geant.csv",
-    "--model_id", f"geant_maskrate_{mask_rate}_{model}_samplenum{samplenum}_seq_{seq_len}",
+    "--model_id", f"geant_maskrate_{mask_rate}_{model}_samplenum{sample_num}_seq_{seq_len}",
+    "--sample_num", str(sample_num),
     "--llm_model", llm_model,
     "--data", "net_traffic_geant",
     "--seq_len", str(seq_len),
-    "--label_len", "0",
-    "--pred_len", str(pred_len),
     "--batch_size", "80",
     "--learning_rate", "0.001",
     '--mlp', "1",
@@ -36,11 +36,9 @@ command = [
     "--d_ff", "768",
     "--enc_in", "96",     # input of enc_embedding && output of flatten head
     "--dec_in", "529",     # feature
-    "--c_out", "529",       # outprojection
+    "--c_out", "529",       # mlp
     "--freq", "h",
-    "--patch_size", "1",
     "--Lambda", str(Lambda),
-    "--stride", "1",
     "--percent", str(percent),
     "--gpt_layer", "6",
     "--model", model,
