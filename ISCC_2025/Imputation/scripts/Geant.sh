fe53584fcc6aa2d1a2f3db78de030b7be92c6cea
FLYingK <118822513+FILingK@users.noreply.github.com>
2025-02-17 21:07:12 +0800

    Update Geant.sh

--- ISCC_2025/Imputation/scripts/Geant.sh
+++ ISCC_2025/Imputation/scripts/Geant.sh
@@ -4,13 +4,12 @@ export TOKENIZERS_PARALLELISM="false"
 
 seq_len=96
 model="TM-LLM"
-pred_len=0
 percent=100
 mask_rate="0.5"
-train_epochs="1"
-itr="1"
+train_epochs="10"
+itr="3"
 llm_model="gpt2"
-samplenum=5000
+sample_num=5000 # number of training samples
 Lambda=5
 
 python -u /root/miniconda3/bin/python run.py \
@@ -20,12 +19,11 @@ python -u /root/miniconda3/bin/python run.py \
     --is_training "1" \
     --root_path "../datasets/net_traffic/GEANT" \
     --data_path "geant.csv" \
-    --model_id "geant_maskrate_${mask_rate}_${model}_samplenum${samplenum}_seq_${seq_len}" \
+    --model_id "geant_maskrate_${mask_rate}_${model}_samplenum${sample_num}_seq_${seq_len}" \
     --llm_model $llm_model \
+    --sample_num $sample_num \
     --data "net_traffic_geant" \
     --seq_len $seq_len \
-    --label_len "0" \
-    --pred_len $pred_len \
     --batch_size "80" \
     --learning_rate "0.001" \
     --mlp "1" \
@@ -36,9 +34,7 @@ python -u /root/miniconda3/bin/python run.py \
     --dec_in "529" \
     --c_out "529" \
     --freq "h" \
-    --patch_size "1" \
     --Lambda $Lambda \
-    --stride "1" \
     --percent $percent \
     --gpt_layer "6" \
     --model $model \
