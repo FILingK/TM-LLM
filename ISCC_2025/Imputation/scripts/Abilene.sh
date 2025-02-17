dabd87aa9539d9d7b82464c1d610853f093e55f5
FLYingK <118822513+FILingK@users.noreply.github.com>
2025-02-17 21:06:54 +0800

    Update Abilene.sh

--- ISCC_2025/Imputation/scripts/Abilene.sh
+++ ISCC_2025/Imputation/scripts/Abilene.sh
@@ -6,16 +6,15 @@ export TOKENIZERS_PARALLELISM=false
 # Set variables
 seq_len=96
 model="TM-LLM"
-pred_len=0
 percent=100
 mask_rate="0.5"
-train_epochs="1"
-itr="1"
+train_epochs="10"
+itr="3"
 llm_model="gpt2"
-samplenum=1000
+sample_num=1000 # number of training samples
 Lambda=2
 
-# Command to run the script
+
 python -u run.py \
     --train_epochs $train_epochs \
     --itr $itr \
@@ -23,12 +22,11 @@ python -u run.py \
     --is_training "1" \
     --root_path "../datasets/net_traffic/Abilene" \
     --data_path "abilene.csv" \
-    --model_id "Abilene_maskrate_${mask_rate}_${model}_samplenum${samplenum}_seq_${seq_len}" \
+    --model_id "Abilene_maskrate_${mask_rate}_${model}_samplenum${sample_num}_seq_${seq_len}" \
+    --sample_num $sample_num \
     --llm_model $llm_model \
     --data "net_traffic_abilene" \
     --seq_len $seq_len \
-    --label_len "0" \
-    --pred_len $pred_len \
     --batch_size "96" \
     --learning_rate "0.001" \
     --mlp "1" \
@@ -40,8 +38,6 @@ python -u run.py \
     --c_out "144" \
     --Lambda $Lambda \
     --freq "h" \
-    --patch_size "1" \
-    --stride "1" \
     --percent $percent \
     --gpt_layer "6" \
     --model $model \
